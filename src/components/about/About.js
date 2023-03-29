0522be17689a8a9a3a320fac6e4b06e2c8263774
kingsley <rawmetal77@gmail.com>
2023-03-29 23:52:18 +0200

    about section added

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./about.css";
+
+const images = [
+  "",
+ 
+];
 
 const About = () => {
-  return (
-    <div>
-      abooout
-    </div>
-  )
-}
+  const [activeIndex, setActiveIndex] = useState(0);
 
-export default About
+  const handleImageChange = () => {
+    setActiveIndex((activeIndex + 1) % images.length);
+  };
+
+  return (
+    <div className="container">
+      <h1>About Us</h1>
+      <img
+        src={images[activeIndex]}
+        alt="About Us"
+        className="image"
+      />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel
+        placerat est, vitae commodo nisi. Aliquam commodo ullamcorper mauris, eu
+        bibendum arcu euismod vel. Donec maximus libero sed est commodo, vel
+        iaculis mauris tincidunt. Sed lobortis sapien id commodo efficitur. Sed
+        vel mauris et dolor commodo aliquam. Fusce venenatis tellus eget ipsum
+        aliquam tristique. Fusce euismod odio a elit blandit consectetur. Ut
+        pharetra commodo sapien sed mollis.
+      </p>
+      <button onClick={handleImageChange}>Next Image</button>
+    </div>
+  );
+};
+
+export default About;
