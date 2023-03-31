6bd2e7cc33ef520e0535f476f933b200ebdc7076
kingsley <rawmetal77@gmail.com>
2023-03-31 12:54:15 +0200

    about updated

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,38 +1,75 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import "./about.css";
+import { Details } from "./Details.js";
 
-const images = [
-  "",
- 
-];
+class About extends Component {
+  render() {
+    return (
+      <div className="About" id="about">
+        <div className="container">
+          <div className="about-image">
+            <img
+              src="https://i.pinimg.com/originals/31/0c/fa/310cfac3d064ea29c2401793496f06ac.png"
+              alt="Masudha"
+              height="300"
+              width="300"
+            />
+          </div>
+          <div className="about-section">
+            <h1 className="about-header">
+              About Me
+              <hr className="divider" />
+            </h1>
 
-const About = () => {
-  const [activeIndex, setActiveIndex] = useState(0);
-
-  const handleImageChange = () => {
-    setActiveIndex((activeIndex + 1) % images.length);
-  };
-
-  return (
-    <div className="container">
-      <h1>About Us</h1>
-      <img
-        src={images[activeIndex]}
-        alt="About Us"
-        className="image"
-      />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel
-        placerat est, vitae commodo nisi. Aliquam commodo ullamcorper mauris, eu
-        bibendum arcu euismod vel. Donec maximus libero sed est commodo, vel
-        iaculis mauris tincidunt. Sed lobortis sapien id commodo efficitur. Sed
-        vel mauris et dolor commodo aliquam. Fusce venenatis tellus eget ipsum
-        aliquam tristique. Fusce euismod odio a elit blandit consectetur. Ut
-        pharetra commodo sapien sed mollis.
-      </p>
-      <button onClick={handleImageChange}>Next Image</button>
-    </div>
-  );
-};
+            <h3 className="about-intro">
+            <p>
+              Hi!<br />I’m kingsley,<br />
+              Junior Full-Stack Developer.<br />
+              I am a career changer who went from building technology right into
+              the innovative field of web development.I am also passionate about
+              creating unique user interfaces and developing solutions that aim
+              for a great user experience.
+            </p>
+            <p>
+              As a junior full-stack developer, my goal is to contribute to the
+              development of dynamic and scalable web applications. I have
+              experience in both front-end and back-end technologies, including
+              HTML, CSS, JavaScript, React, Node.js, Express, and MongoDB.
+            </p>
+            <p>
+              I am a quick learner and am always eager to expand my knowledge
+              and skillset. I am committed to writing clean, well-documented,
+              and maintainable code. I am able to work well in a team
+              environment and can effectively communicate with both technical
+              and non-technical stakeholders.
+            </p>
+            <p>
+              In my previous projects, I have successfully implemented features
+              such as user authentication and dynamic data visualization. I have
+              also gained experience in deploying applications to various cloud
+              platforms, such as Heroku and Netlify.
+            </p>
+            <p>
+              I am confident in my ability to take on challenging projects and
+              am excited to continue my growth as a full-stack developer.
+            </p>
+            </h3>
+            <br />
+            <ul className="about-details">
+              {Details.map((item, index) => {
+                return (
+                  <li className="about-details-list" key={index}>
+                    <span className="about-title">{item.title} </span>
+                    <span className="title.value">{item.value} </span>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default About;
---
+++ src/components/about/Details.js
@@ -0,0 +1,14 @@
+export const Details = [
+  {
+    title: "Name: ",
+    value: "Masudha Meher"
+  },
+  {
+    title: "Email: ",
+    value: "rawkingsley@gmail.com"
+  },
+  {
+    title: "City: ",
+    value: "Berlin, Germany."
+  }
+];
